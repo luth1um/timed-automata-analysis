36946df1c6559eb2fe733ae94c35411e6dcbfe2c
luth1um <52032867+luth1um@users.noreply.github.com>
2024-03-25 10:58:47 +0100

    configured path for project

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/timed-automata-analysis/',
+  build: {
+    outDir: 'dist/timed-automata-analysis',
+  },
+  server: {
+    open: true,
+  },
 });
