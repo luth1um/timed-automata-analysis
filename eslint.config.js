80801c97996e02264719522e6f3a93f32d38393f
luth1um <52032867+luth1um@users.noreply.github.com>
2024-04-13 16:27:04 +0200

    update ESLint config format

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    // eslint-plugin-react-hooks
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    // eslint-plugin-react-refresh
+    plugins: {
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+  {
+    // eslint-plugin-unused-imports
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+  },
+  {
+    // eslint-plugin-unused-imports
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    // needs to be in its own object to act as global ignore
+    ignores: ['dist', '*.cjs'],
+  }
+);
