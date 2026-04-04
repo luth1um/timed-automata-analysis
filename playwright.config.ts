18518475bbc91d9ad41e6f37c9f7faa8d930d7be
luth1um <52032867+luth1um@users.noreply.github.com>
2026-04-04 14:20:05 +0200

    parallelized tests for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* No retries (not even on CI) */
   retries: 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Parallel tests on CI. */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'blob' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
